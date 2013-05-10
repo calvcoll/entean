a2cb8ea2baff99d2908479ce7c7524d698b4a4b5
Calv Collins <calvcoll@gmail.com>
2013-05-10 22:13:24 +0100

    Initial commit

---
+++ entean.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+entean = ['a','z','y','x','e','w','v','t','i','s','r','l','p','n','o','m','q','k','j','h','u','g','f','d','c','b']
+english = ('a'..'z').to_a
+
+puts "Translate: "
+input = gets.downcase
+
+output = []
+input.each_char do |letter|
+  index = english.index(letter)
+  unless index.nil?
+    output.push(entean[index])
+  else
+    output.push(letter)
+  end
+end
+
+puts "#{output.join}"
